b4abb4be6b08bd7b592271ff14303713ad510953
ThisaraJayas <sasmithajayasinghe1@gmail.com>
2023-11-23 19:43:05 +0530

    new

---
+++ src/main/java/com/Thisara/api/MyNewProjectApplication.java
@@ -0,0 +1,12 @@
+package com.Thisara.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNewProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyNewProjectApplication.class, args);
+	}
+
+}
