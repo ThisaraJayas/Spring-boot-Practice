cbc1f50dac341a3012a1e1047b4c3f3f9f5f8d92
ThisaraJayas <sasmithajayasinghe1@gmail.com>
2023-11-23 23:53:27 +0530

    new

--- src/main/java/com/Thisara/api/InvestorService.java
+++ src/main/java/com/Thisara/api/InvestorService.java
@@ -9,7 +9,7 @@ public class InvestorService {
     EligibilityService es;
     String Register(String name, float registerAmount){
 
-        boolean result = es.isEligible(registerAmount);nn
+        boolean result = es.isEligible(registerAmount);
 
         if(result){
             return "Hi "+name+" Welcome";
